0eb00aed583de43f33f8cbad51a8e106ed824d4e
KaLen9ula <andruhovickola167@gmail.com>
2021-02-19 16:58:11 +0200

    Create project for lab work 1

---
+++ app/src/main/java/com/example/uakpicomsysio8101/ui/dashboard/GraphicFragment.kt
@@ -0,0 +1,20 @@
+package com.example.uakpicomsysio8101.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.uakpicomsysio8101.R
+
+class GraphicFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_graphic, container, false)
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/uakpicomsysio8101/ui/home/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.uakpicomsysio8101.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.uakpicomsysio8101.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        return root
+    }
+}
